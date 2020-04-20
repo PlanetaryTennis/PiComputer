f80e1c33211553218b2bb82f773715565a6de264
PlanetaryTennis <PlanetaryTennis@gmail.com>
2020-04-19 22:18:59 -0400

    sh measures

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
+echo :---: >> measures.txt
+echo Looking for $3 >> measures.txt
+echo With $1 proccesses >> measures.txt
+date +%T.%N >> measures.txt
 mpiexec -f /home/pi/machinefile -n $1 /home/pi/$2 $3 > $3.txt
+date +%T.%N >> measures.txt
